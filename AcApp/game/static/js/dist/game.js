77caf36bfc861ccfbb11227f0597f08033ac5dff
daozuqiexing <990758914@qq.com>
2022-07-22 23:26:09 +0800

    enable fireball

--- AcApp/game/static/js/dist/game.js
+++ AcApp/game/static/js/dist/game.js
@@ -148,6 +148,8 @@ class Player extends AcGameObject {
         this.speed = speed; // 每秒钟移动 speed
         this.is_me = is_me;
         this.eps = 0.1; // 移动误差
+
+        this.cur_skill = null; // 当前选择的技能是什么
     }
 
     start() {
@@ -165,7 +167,33 @@ class Player extends AcGameObject {
             if (e.which === 3) { // e.which === 3 表示右键，e.which === 1 表示左键，e.which === 2 表示滚轮
                 outer.move_to(e.clientX, e.clientY); // e.clientX 表示鼠标点击的横坐标，e.clientY 表示鼠标点击的纵坐标
             }
+            else if (e.which === 1) {
+                if (outer.cur_skill === "fireball") {
+                    outer.shoot_fireball(e.clientX, e.clientY);
+                }
+
+                outer.cur_skill = null;
+            }
         });
+
+        $(window).keydown(function(e) {
+            if (e.which === 81) { // Q 按键的 keycode
+                outer.cur_skill = "fireball";
+                return false; // 表示后续不处理
+            }
+        });
+    }
+
+    shoot_fireball(tx, ty) { // 施放火球，参数为火球发射的方向
+        let x = this.x, y = this.y;
+        let radius = this.playground.height * 0.01;
+        let angle = Math.atan2(ty - this.y, tx - this.x);
+        let vx = 1 * Math.cos(angle);
+        let vy = 1 * Math.sin(angle);
+        let color = "orange";
+        let speed = this.playground.height * 0.5;
+        let move_length = this.playground.height * 1;
+        new FireBall(this.playground, this, x, y, radius, vx, vy, color, speed, move_length);
     }
 
     get_dist(x1, y1, x2, y2) { // 获取当前坐标到目标坐标的欧几里德距离
@@ -177,10 +205,10 @@ class Player extends AcGameObject {
     move_to(tx, ty) { // 移动 player，参数为目的坐标的横坐标和纵坐标
         this.move_length = this.get_dist(this.x, this.y, tx, ty); // 求出当前坐标到目标坐标的距离
         let angle = Math.atan2(ty - this.y, tx - this.x); // 求出当前坐标到目标坐标的角度，参数依次为：y 方向的偏移量，x 方向的偏移量
-        this.vx = 1 * Math.cos(angle); // x 方向上的速度
+        this.vx = 1 * Math.cos(angle); // x 方向上的速度, 实际上就是方向
         this.vy = 1 * Math.sin(angle); // y 方向上的速度
     }
-z
+
     update() {
         if (this.move_length < this.eps) { // 小于误差时，不需要移动了
             this.move_length = 0;
@@ -188,10 +216,51 @@ z
         }
         else {
             let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000); // 每两帧之间移动多少
-            this.x += this.vx * moved; // x 方向上每两帧之间移动的距离
+            this.x += this.vx * moved; // x 方向上每两帧之间移动的距离，方向乘以距离
             this.y += this.vy * moved; // y 方向上每两帧之间移动的距离
             this.move_length -= moved; // 减去已经移动的距离
         }
+        this.render();
+    }
+
+    render() {
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+}
+class FireBall extends AcGameObject {
+    constructor(playground, player, x, y, radius, vx, vy, color, speed, move_length) {
+        super();
+
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.move_length = move_length;
+        this.eps = 0.1;
+    }
+
+    start() {
+
+    }
+
+    update() {
+        if (this.move_length < this.eps) {
+            this.destroy();
+            return false;
+        }
+
+        let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
+        this.x += this.vx * moved; // 方向乘以距离
+        this.y += this.vy * moved;
+        this.move_length -= moved;
         this.render();
     }
 
